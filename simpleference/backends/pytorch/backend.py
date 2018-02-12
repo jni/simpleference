268aebb885ce1782d6e65517d9b209351000de0b
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-02-13 01:27:38 +1100

    Use queue of models to keep available GPUs busy
    By using a Queue of GPU/model pairs within PyTorchPredict, we can use
    GPUs as they become available, rather than pre-assign blocks to GPUs.

--- simpleference/backends/pytorch/backend.py
+++ simpleference/backends/pytorch/backend.py
@@ -3,24 +3,34 @@ import numpy as np
 
 import dill
 import torch
+from contextlib import contextmanager
 from torch.autograd import Variable
-import threading
+
+from Queue import Queue
 
 
 class PyTorchPredict(object):
-    def __init__(self, model_path, crop=None, gpu=0):
+    def __init__(self, model_path, crop=None, gpu_list=[0]):
         assert os.path.exists(model_path), model_path
-        self.model = torch.load(model_path, pickle_module=dill)
-        # NOTE we always set CUDA_VISIBLE_DEVICES to our desired gpu
-        # so we can always assume gpu 0 here
-        self.gpu = gpu
-        self.model.cuda(self.gpu)
+        if np.isscalar(gpu_list):
+            gpu_list = [gpu_list]
+        self.model_queue = Queue(maxsize=len(gpu_list))
+        for gpu in gpu_list:
+            # load and copy the model to all GPUs.
+            model = torch.load(model_path, pickle_module=dill)
+            model.cuda(gpu)
+            self.model_queue.put((gpu, model))
         # validate cropping
         if crop is not None:
             assert isinstance(crop, (list, tuple))
             assert len(crop) == 3
         self.crop = crop
-        self.lock = threading.Lock()
+
+    @contextmanager
+    def get_gpu_model(self):
+        gpu, model = self.model_queue.get()
+        yield gpu, model
+        self.model_queue.put((gpu, model))
 
     def apply_crop(self, out):
         shape_diff = tuple((shape - crop) // 2
@@ -36,13 +46,13 @@ class PyTorchPredict(object):
         # better performance by only locking in step 2, or steps 1-2, or steps
         # 2-3. We should perform this experiment and then choose the best
         # option for our hardware (and then remove this comment! ;)
-        with self.lock:
+        with self.get_gpu_model() as gpu, model:
             # 1. Transfer the data to the GPU
             torch_data = Variable(torch.from_numpy(input_data[None, None])
-                                  .cuda(self.gpu), volatile=True)
+                                  .cuda(gpu), volatile=True)
             print('predicting a block!')
             # 2. Run the model
-            predicted_on_gpu = self.model(torch_data)
+            predicted_on_gpu = model(torch_data)
             # 3. Transfer the results to the CPU
             out = predicted_on_gpu.cpu().data.numpy().squeeze()
         if self.crop is not None:
